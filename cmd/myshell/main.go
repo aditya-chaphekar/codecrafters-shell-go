ae5b2d090178ab7692a51ea6ecba0c52e435d7cc
Aditya Chaphekar <adityachaps@gmail.com>
2025-02-13 12:47:35 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,7 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func EvaluteCmd(cmd string) {
+	c := strings.Split(cmd, " ")
+	switch c[0] {
+		case "exit":
+			code, _ := strconv.Atoi(c[1])
+			os.Exit(code)
+			break
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd[:len(cmd)-1])
+	}
+}
+
 
 func Repl() {
 	for true {
@@ -16,8 +31,7 @@ func Repl() {
 			fmt.Fprintln(os.Stderr, "error reading input:", err)
 			os.Exit(1)
 		}
-
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd[:len(cmd)-1])
+		EvaluteCmd(cmd)
 	}
 }
 
