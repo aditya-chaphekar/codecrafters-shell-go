3ed3de11d6d34a0cf1d4fb69a12c0ba03b2b9d00
Aditya Chaphekar <adityachaps@gmail.com>
2025-02-14 14:52:31 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,11 +5,44 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 	"strconv"
 	"strings"
 )
 
-var builtins = []string{"echo", "exit", "type"}
+var defaultPrmopt = true
+
+type BuiltinCmds struct {
+	name string
+}
+
+func (cmd BuiltinCmds) exex(args []string) {
+	switch cmd.name {
+	case "exit":
+		HandleExit(args)
+		break
+
+	}
+}
+
+var builtin []BuiltinCmds = []BuiltinCmds{
+	{name: "echo"},
+	{name: "exit"},
+	{name: "quit"},
+	{name: "type"},
+	{name: "pwd"},
+}
+var progs []map[string]string
+
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var Blue = "\033[34m"
+var Magenta = "\033[35m"
+var Cyan = "\033[36m"
+var Gray = "\033[37m"
+var White = "\033[97m"
 
 func GetAvailableProgramsFromPath() []map[string]string {
 	env, err := os.LookupEnv("PATH")
@@ -34,59 +67,110 @@ func GetAvailableProgramsFromPath() []map[string]string {
 	return programsInPath
 }
 
+func HandleExit(arg []string) {
+	code := 1
+	if len(arg) <= 0 {
+		code = 0
+	} else {
+		code, _ = strconv.Atoi(arg[0])
+	}
+	os.Exit(code)
+	return
+}
+
+func isBuiltinCommand(cmd string) bool {
+	for _, v := range builtin {
+		if v.name == cmd {
+			return true
+		}
+	}
+	return false
+}
+
+func isExternalCommand(cmd string) (bool, string) {
+	for _, p := range progs {
+		if p["name"] == cmd {
+			return true, p["path"]
+		}
+	}
+	return false, ""
+}
+
+func HandleEcho(args []string) {
+	fmt.Fprint(os.Stdout, strings.Join(args, " "), "\n")
+}
+
+func HandleType(args []string) {
+	cmdToCheck := strings.Join(args, " ")
+	if isBuiltinCommand(cmdToCheck) {
+		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", cmdToCheck)
+	} else if ok, p := isExternalCommand(cmdToCheck); ok {
+		fmt.Fprintf(os.Stdout, "%s is %s\n", cmdToCheck, p)
+	} else {
+		fmt.Fprintf(os.Stdout, "%s: not found\n", cmdToCheck)
+	}
+}
+
+func HandlePwd() {
+	workinDir, _ := os.Getwd()
+	fmt.Fprintf(os.Stdout, "%s\n", workinDir)
+}
+
 func EvaluteCmd(cmd string) {
 	c := strings.Split(cmd, " ")
-	progs := GetAvailableProgramsFromPath()
-	switch c[0] {
-	case "exit":
-		code := 1
-		if len(c) == 1 {
-			code = 0
-		} else {
-			code, _ = strconv.Atoi(c[1])
-		}
-		os.Exit(code)
-		break
-	case "echo":
-		cmdLen := len(c[0])
-		fmt.Fprintf(os.Stdout, "%s\n", cmd[cmdLen+1:])
-	case "type":
-		arg := cmd[5:]
-
-		for _, b := range builtins {
-			if b == arg {
-				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", arg)
-				return
-			}
-		}
-		for _, p := range progs {
-			if p["name"] == arg {
-				fmt.Fprintf(os.Stdout, "%s is %s\n", arg, p["path"])
-				return
-			}
-		}
-		fmt.Fprintf(os.Stdout, "%s: not found\n", arg)
-	default:
-		cmdLen := len(c[0])
-		args := make([]string, 0)
-		if cmdLen < len(cmd) {
-			args = strings.Split(strings.TrimSpace(cmd[cmdLen:]), " ")
-		}
-		for _, p := range progs {
-			if p["name"] == c[0] {
-				exeCmd := exec.Command(p["name"], args...)
-				out, _ := exeCmd.Output()
-				fmt.Fprint(os.Stdout, string(out))
-				return
-			}
-		}
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+	command := c[0]
+	cmdLen := len(command)
+	args := make([]string, 0)
+	if cmdLen < len(cmd) {
+		args = strings.Split(strings.TrimSpace(cmd[cmdLen:]), " ")
 	}
+	if isBuiltinCommand(command) {
+		if command == "exit" || command == "quit" {
+			HandleExit(args)
+			return
+		} else if command == "echo" {
+			HandleEcho(args)
+			return
+		} else if command == "type" {
+			HandleType(args)
+			return
+		} else if command == "pwd" {
+			HandlePwd()
+			return
+		}
+		return
+	} else if ok, _ := isExternalCommand(command); ok {
+		exeCmd := exec.Command(command, args...)
+		out, _ := exeCmd.Output()
+		fmt.Fprint(os.Stdout, string(out))
+		return
+	}
+	fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+}
+
+func Prompt() {
+	user, _ := os.LookupEnv("USER")
+	hostnameCmd := exec.Command("hostname")
+	hostname, _ := hostnameCmd.Output()
+	workinDir, _ := os.Getwd()
+	dirShortHome := strings.ReplaceAll(workinDir, "/home/"+user, "~")
+	splitDir := strings.Split(dirShortHome, "/")
+	dirWithoutCurrentPath := splitDir[:len(splitDir)-1]
+	dir := ""
+	for _, v := range dirWithoutCurrentPath {
+		dir += string([]rune(v)[0]) + "/"
+	}
+	dir += splitDir[len(splitDir)-1]
+	fmt.Fprintf(os.Stdout, Yellow+"%s"+Reset+" at "+Magenta+"%s"+Reset+" in "+Green+"%s"+Reset+" \n↪  ", user, strings.ReplaceAll(string(hostname), "\n", ""), dir)
 }
 
 func Repl() {
 	for true {
-		fmt.Fprint(os.Stdout, "$ ")
+		if defaultPrmopt {
+			fmt.Fprint(os.Stdout, "$ ")
+		} else {
+			Prompt()
+		}
 
 		// Wait for user input
 		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
@@ -98,8 +182,33 @@ func Repl() {
 	}
 }
 
+func ClearScreen() {
+	clear := make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+
+}
+
 func main() {
-	// Uncomment this block to pass the first stage
+	if !defaultPrmopt {
+		ClearScreen()
+	}
+	progs = GetAvailableProgramsFromPath()
 	Repl()
 
 }
